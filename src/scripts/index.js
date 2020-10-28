fee1efb56f4e0a918acab5275527eb09a617344a
Никита Воскобойников <38068@SYS-231.chisw.com>
2020-10-28 16:57:36 +0200

    added delete and post request

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,5 @@
 import '../styles/index.scss';
 import './modules/script.js';
-import './modules/templateBook.js';
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
--- src/scripts/modules/script.js
+++ src/scripts/modules/script.js
@@ -5,20 +5,37 @@ let cancelBtn = document.querySelector(".cancel_btn");
 let modalWindow = document.querySelector(".modal_window_background");
 let sectionForBooks = document.querySelector(".section_books");
 
+let modalWindowName = document.querySelector("#name");
+let modalWindowAuthor = document.querySelector("#author");
+let modalWindowPages = document.querySelector("#pages");
+let modalWindowSize = document.querySelector("#size");
+let modalWindowQuality = document.querySelector("#quality");
+let modalWindowLanguage = document.querySelector("#language");
+let modalWindowYear = document.querySelector("#year");
+let modalWindowImg = document.querySelector("#img");
+let modalWindowDescription = document.querySelector("#modal_window_description");
+let create_btn = document.querySelector(".create_btn");
+
+
 addBookBtn.addEventListener("click", () => {
 modalWindow.style.display = "block";
 });
 
-cancelBtn.addEventListener("click", () => {
-    modalWindow.style.display = "none";
-    });
-    window.addEventListener("click", (e) => {
+cancelBtn.addEventListener("click", () => closeModal());
+
+window.addEventListener("click", (e) => {
         if(e.target === modalWindow){
-            modalWindow.style.display = "none";
+            closeModal();
         }
     });
 
+const closeModal = () => {
+    modalWindow.style.display = "none";
+};
+
+//GET REQUEST
     function getBooks(){
+        sectionForBooks.innerHTML = "";
         fetch(myUrl)
         .then(response => response.json())
         .then(books => {
@@ -47,8 +64,50 @@ cancelBtn.addEventListener("click", () => {
                 bookYear.innerHTML = `<b>Год: </b>${oneBook.yearNode}`;
                 bookDescription.innerHTML = `<b>Описание: </b>${oneBook.descriptionNode}`;
 
+                let changeBtn = book.querySelector(".change");
+                let deleteBtn = book.querySelector(".delete");
+                deleteBtn.addEventListener("click", () => deleteBook(oneBook.id));
                 sectionForBooks.appendChild(book);
             });
         });
     }
     getBooks();
+
+//POST REQUEST
+    create_btn.addEventListener("click", () => {
+        let post = {
+            imgNode: modalWindowImg.value,
+            nameNode: modalWindowName.value,
+            authotNode: modalWindowAuthor.value,
+            pagesNode: +modalWindowPages.value,
+            sizeNode: modalWindowSize.value,
+            qualityNode: modalWindowQuality.value,
+            languageNode: modalWindowLanguage.value,
+            yearNode: +modalWindowYear.value,
+            descriptionNode: modalWindowDescription.value
+        };
+        fetch(myUrl, {
+            method: 'POST',
+            body: JSON.stringify(post),
+            headers: {
+                        'Content-Type': 'application/json',
+                     }
+        })
+        .then(closeModal)
+        .then(getBooks);
+    });
+
+//DELETE REQUEST
+     function deleteBook(id){
+        fetch(`${myUrl}/${id}`, {
+            method: "DELETE"
+        })
+        .then(getBooks);
+     }
+
+
+
+
+
+
+
